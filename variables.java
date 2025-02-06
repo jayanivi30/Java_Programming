80b4305956d8077fb23457a0b924a2bd371553ca
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2025-02-06 23:03:53 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,21 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name= "Jayastri Nivedha";
+		int age = 20;
+		double cgpa = 8.9;
+		char gender = 'F';
+		boolean isStudent = true;
+		System.out.println("Hii I'm " + name);
+		System.out.println("I'm " + age + " years old");
+		System.out.println("My current cgpa is " + cgpa);
+		System.out.println("Is student? " + isStudent);
+		
+		
+		
+
+	}
+
+}
