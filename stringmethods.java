8745cff8e1f9cc8201b7aed6d9e4ffda1430a157
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2025-03-03 23:20:45 +0530

    Add files via upload

---
+++ stringmethods.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class stringmethods {
+
+	public static void main(String[] args) {
+		String name = "  JayaNivi  ";
+		System.out.println(name.toUpperCase());
+		System.out.println(name.toLowerCase());
+		System.out.println(name.length());
+		System.out.println(name.trim());
+		System.out.println(name.charAt(6));
+		System.out.println(name.indexOf("J"));
+		System.out.println(name.isEmpty());
+		
+
+	}
+
+}
